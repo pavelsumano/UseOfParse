1e9d56169a62d770b28104504bfd9061e5dd1881
sumanoop <sumanoop@iMac-de-sumanoop.local>
2015-02-19 17:09:33 -0600

    Initial Commit

---
+++ UseOfParse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseOfParse
+//
+//  Created by sumanoop on 19/02/15.
+//  Copyright (c) 2015 Pavel sumano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UseOfParse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UseOfParse
+//
+//  Created by sumanoop on 19/02/15.
+//  Copyright (c) 2015 Pavel sumano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
